b2f95749edd5da557ae92940a553220ee27c53c6
Nathan Smith <nathan@smithfam.info>
2012-03-12 10:52:50 -0700

    Added Words of Jesus script

---
+++ wordsofjesus.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+#
+# Script to extract and rank all of Jesus words
+# (c) 2012 Nathan Smith <nathan@smithfam.info>
+
+import logging
+import os
+import StringIO
+import urllib
+import xml.sax
+import zipfile
+
+import nltk
+from nltk.corpus import stopwords
+stops = stopwords.words('english')
+
+url = 'http://ebible.org/web/eng-web_usfx.zip'
+
+def nonstopword(word):
+    if word in stops:
+        return False
+    else:
+        return True
+
+class WEBParser(xml.sax.handler.ContentHandler):
+    """Class to parse WEB XML and extract words of Jesus"""
+
+    def __init__(self):
+        self.in_wj = False
+        self.words = ""
+
+    def startElement(self,name, attrs):
+        if name == "wj":
+            self.in_wj = True
+
+    def characters(self, data):
+        if self.in_wj:
+            self.words += data.lower()
+
+    def endElement(self, name):
+        if name == "wj":
+            self.in_wj = False
+
+if __name__ == '__main__':
+    if not os.path.exists(os.path.basename(url)):
+        try:
+            urllib.urlretrieve(url, os.path.basename(url))
+        except:
+            sys.exit(1)
+
+    web_zip = os.path.basename(url)
+    parser = xml.sax.make_parser()
+    web = WEBParser()
+    parser.setContentHandler(web)
+    _zip = zipfile.ZipFile(web_zip)
+    parser.parse(StringIO.StringIO(_zip.read("eng-web_usfx.xml")))
+    words_of_jesus = nltk.word_tokenize(web.words)
+    word_list = [w for w in words_of_jesus if (nonstopword(w) and w.isalpha())]
+    fdist = nltk.FreqDist(word_list)
+    for word in fdist.keys()[:100]:
+        print word
